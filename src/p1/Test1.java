ea454657ec555edde88414334c1fd9ffd859f2cd
Sandip Patil <sandipvitthalpatil@gmail.com>
2022-01-23 08:31:30 +0530

    Project Upload

---
+++ src/p1/Test1.java
@@ -0,0 +1,8 @@
+package p1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("In p1 test1");
+	}
+}
---
+++ src/p2/Test2.java
@@ -0,0 +1,8 @@
+package p2;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		System.out.println("In p2 test2");
+	}
+}
